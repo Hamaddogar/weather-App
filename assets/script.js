4d6bab6dacf10d77c89c1075f7ad8ece204cf0c7
Muhammad Hamad <hamadali77115033@gmail.com>
2024-01-14 21:36:42 +0500

    city display element

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,11 @@ const APIkey = "f792ea86435d6d03093ac74cf0bb3026";
 const formTagEl = document.getElementById('form-test');
 const searchHistoryEl = document.getElementById('search-history');
 const cityNameEl = document.getElementById('city');
+//City Display Elements
+const currentTempEl = document.getElementById('temperature');
+const currentCityEl = document.getElementById('city');
+const currentWindSpeedEl = document.getElementById('wind-speed');
+const currentHumidityEl = document.getElementById('humid');
+const searchInputEl = document.getElementById("input");
+const fiveDayEl = document.querySelector('.five-day');
+const singleDayIcon = document.getElementById('single-day-icon');
