cf645c2fa1cff630c48653bd42fe390011f99bfc
Muhammad Hamad <hamadali77115033@gmail.com>
2024-01-14 21:35:14 +0500

    element  import in js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,4 @@
 const APIkey = "f792ea86435d6d03093ac74cf0bb3026";
+const formTagEl = document.getElementById('form-test');
+const searchHistoryEl = document.getElementById('search-history');
+const cityNameEl = document.getElementById('city');
